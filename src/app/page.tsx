eca376f6de2a1f9448f2f2fa2e6049109a8b2260
Liam Gessman <Liamngessman+frc@gmail.com>
2025-05-30 22:24:12 -0400

    added cool gradiant thing around mouse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import CursorGlow from './components/CursorGlow';
 
 export default function Home() {
   return (
+    <CursorGlow>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sans-serif)]">
       <header className="row-start-1 flex items-center center">
         <div className ="text-6xl font-bold tracking-[-.01em]">
@@ -23,7 +25,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-15 sm:h-17 px-4 sm:px-5 sm:w-auto"
             href="https://github.com/Horizon-Avionics"
             target="_blank"
             rel="noopener noreferrer"
@@ -38,7 +40,7 @@ export default function Home() {
             Check out the repositories!
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-15 sm:h-17 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://docs.google.com/document/d/1rhxR0z3o1-5nPLaZscAGfexemYaoFTkNt6UDeAldLvw/edit?pli=1&tab=t.0"
             target="_blank"
             rel="noopener noreferrer"
@@ -51,5 +53,6 @@ export default function Home() {
         
       </footer>
     </div>
+    </CursorGlow>
   );
 }
